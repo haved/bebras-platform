621965e1ef01bb917d50b45ed53ae40afd19447f
haved <krogstie.havard@gmail.com>
2022-11-04 00:09:33 +0100

    Use the same config as before, but allow env var override

--- config_local.php
+++ config_local.php
@@ -8,9 +8,9 @@ $config->db->dynamoDBPrefix = ''; // prefix for sessions, team and team_question
 
 // MySQL
 $config->db->mysql->host = '127.0.0.1';
-$config->db->mysql->database = 'beaver_contest';
-$config->db->mysql->user = '';
-$config->db->mysql->password = '';
+$config->db->mysql->database = 'bebras';
+$config->db->mysql->user = 'bebras';
+$config->db->mysql->password = getenv('BEBRAS_DB_PASSWORD');
 $config->db->mysql->logged = false;
 
 // AWS, if relevant
@@ -30,14 +30,16 @@ $config->email->sInfoAddress = 'nio@nio.no';
 
 // Localization
 $config->timezone = 'Europe/Oslo';
-$config->defaultLanguage = 'fr';
-$config->teacherInterface->countryCode = 'FR';
+$config->defaultLanguage = 'no';
+$config->teacherInterface->countryCode = 'NO';
+
+$baseURL = getenv('BEBRAS_BASE_URL') ?: 'https://olympiade.ii.uib.no/runde1';
 
 // Teacher interface settings
-$config->teacherInterface->sCoordinatorFolder = 'http://127.0.0.1/beaver_platform/teacherInterface/';
-$config->teacherInterface->sAssetsStaticPath = 'http://127.0.0.1/beaver_platform/contestInterface/';
-$config->teacherInterface->sAbsoluteStaticPath = 'http://127.0.0.1/beaver_platform/contestInterface/';
-$config->teacherInterface->sAbsoluteStaticPathOldIE = 'http://127.0.0.1/beaver_platform/contestInterface/';
+$config->teacherInterface->sCoordinatorFolder = "$baseURL/teacherInterface/";
+$config->teacherInterface->sAssetsStaticPath = "$baseURL/";
+$config->teacherInterface->sAbsoluteStaticPath = "$baseURL/";
+$config->teacherInterface->sAbsoluteStaticPathOldIE = "$baseURL/";
 $config->teacherInterface->genericPasswordMd5 = '';
 $config->teacherInterface->generationMode = 'local';
 $config->teacherInterface->sContestGenerationPath = '/../contestInterface/contests/'; // *MUST* be relative!
@@ -45,27 +47,28 @@ $config->teacherInterface->forceOfficialEmailDomain = false;
  // indicate the ID of the contest for which password will be automatically generated for teachers
 $config->teacherInterface->teacherPersonalCodeContestID = 0;
 
-$config->contestInterface->sAssetsStaticPathNoS3 = 'http://concours.castor-informatique.fr';
-$config->contestInterface->sAbsoluteStaticPathNoS3 = 'http://concours.castor-informatique.fr';
+$config->contestInterface->sAssetsStaticPathNoS3 = "$baseURL";
+$config->contestInterface->sAbsoluteStaticPathNoS3 = "$baseURL";
 
-$config->grades = [-1,3,4,5,6,7,8,9,10,11,12,-4];
+
+$config->grades = [-1,3,4,5,6,7,8,9,10,11,12,13,-4];
 $config->defaultCategory = '';
 $config->defaultTeacherCategory = '';
 $config->trainingContestID = '56';
 
 
 // URLs
-$config->teacherInterface->baseUrl = 'http://coordinateur.castor-informatique.fr/';
-$config->contestInterface->baseUrl = 'http://concours.castor-informatique.fr/';
-$config->certificates->webServiceUrl = 'http://castor-informatique.fr.localhost/certificates/';
-$config->contestPresentationURL = 'http://castor-informatique.fr/';
-$config->contestOfficialURL = 'http://concours.castor-informatique.fr/';
+$config->teacherInterface->baseUrl = "${baseURL}adm/";
+$config->contestInterface->baseUrl = "$baseURL/";
+$config->certificates->webServiceUrl = "$baseURL/certificates";
+$config->contestPresentationURL =  "$baseURL/";
+$config->contestOfficialURL = "$baseURL/";
 $config->contestBackupURL = '';
 $config->useCustomStrings = false; // see README
 
 // Timestamp for URLs
 // If defined, ?v=[timestamp] will be added to all URLs.
-$config->timestamp = 1000000000;
+$config->timestamp = 1000000001;
 
 // Should we upgrade contest URLs (resources) to HTTPS client-side?
 // If true, upgrade all URLs; if an array, upgrade all URLs from these domains
